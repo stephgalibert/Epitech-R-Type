eb3ed757cb0ac1356b000473f799d09290794fb2
Clement-Oliva <clement.oliva@epitech.eu>
2016-12-12 22:52:38 +0100

    Menu Server Browser Base

--- client/rtype/rtype/MainMenuController.hpp
+++ client/rtype/rtype/MainMenuController.hpp
@@ -5,6 +5,7 @@
 
 #include "AController.hpp"
 #include "MenuButton.hpp"
+#include "MenuServerBrowser.hpp"
 
 class MainMenuController : public AController
 {
@@ -21,6 +22,10 @@ class MainMenuController : public AController
 	static const uint32_t SPLASH_INIT_POS;
 	static const uint32_t SPLASH_TITLE_TARGET_POS;
 	static const float KEYBOARD_EVENT_DELTA_MIN;
+	static const float SERVER_BROWSER_POS_X;
+	static const float SERVER_BROWSER_POS_Y;
+	static const float SERVER_BROWSER_WIDTH;
+	static const float SERVER_BROWSER_HEIGHT;
 
 public:
 	MainMenuController(void);
@@ -33,13 +38,16 @@ public:
 		ST_Splash1 = 1,
 		ST_Splash2 = 2,
 		ST_Splash3 = 3,
-		ST_Menu = 4
+		ST_Splash4 = 4,
+		ST_Menu = 10,
+		ST_Selecting = 20
 	};
 
 public:
 	enum SelectedAction : short {
 		PLAY = 0,
-		QUIT = 1,
+		CREATE = 1,
+		QUIT = 2,
 		NONE = -1
 	};
 
@@ -60,6 +68,7 @@ private:
 	void updateSplashFirstPhase(const float delta);
 	void updateSplashSecondPhase(const float delta);
 	void updateSplashThirdPhase(const float delta);
+	void updateSplashFourthPhase(const float delta);
 	void updateMenu(const float delta);
 	void abortSplash(void);
 
@@ -74,8 +83,15 @@ private:
 	float _keyboardEventDelta;
 	std::unordered_map<uint32_t, std::function<bool(void)>> _keyActions;
 	short _pushAction;
+	uint8_t _buttonsAlpha;
+	int _selectedServer;
 
 private:
 	std::vector<sf::Sprite> _titleSprites;
 	std::vector<MenuButton> _buttons;
+	MenuServerBrowser _browser;
+
+	/* TEST ONLY */
+private:
+	std::vector<std::string> _browserContent;
 };
