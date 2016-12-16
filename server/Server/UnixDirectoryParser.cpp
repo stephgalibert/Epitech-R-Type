5f56e87dead98bc7f0c62f56c29fe6e0d16ba4bc
galibe_s <stephane.galibert@epitech.eu>
2016-12-16 23:11:40 +0100

    add unix directory parser

--- server/Server/UnixDirectoryParser.cpp
+++ server/Server/UnixDirectoryParser.cpp
@@ -12,5 +12,20 @@ UnixDirectoryParser::~UnixDirectoryParser(void)
 
 void UnixDirectoryParser::parse(void)
 {
+  size_t pos;
+  DIR *dir;
+  struct dirent *ent;
 
+  if ((dir = opendir(this->getDirPath().c_str())) != NULL) {
+    while ((ent = readdir(dir)) != NULL)
+      if (ent->d_name[0] != '.') {
+	std::string path(ent->d_name);
+        pos = path.find_last_of(".");
+	if (pos != std::string::npos && path.substr(pos) == ".so") {
+	  this->addFile(path);
+	  std::cout << ent->d_name << " added" << std::endl;
+	}
+      }
+    closedir(dir);
+  }
 }
