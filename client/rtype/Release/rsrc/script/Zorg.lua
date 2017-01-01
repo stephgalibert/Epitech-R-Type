231f4a5ef380cf5108371dc58719710f5ac86254
galibe_s <stephane.galibert@epitech.eu>
2017-01-01 18:00:00 +0100

    installeur

---
+++ client/rtype/Release/rsrc/script/Zorg.lua
@@ -0,0 +1,20 @@
+function getTarget(mobPosX, mobPosY, p0PosX, p0PosY, p1PosX, p1PosY, p2PosX, p2PosY, p3PosX, p3PosY)
+	posArray =  {
+		{p0PosX, p0PosY},
+		{p1PosX, p1PosY},
+		{p2PosX, p2PosY},
+		{p3PosX, p3PosY}
+	}
+	indexToRet = 1
+	nearestTarget = math.abs(mobPosX - p0PosX) + math.abs(mobPosY - p0PosY)
+	for i, v in ipairs(posArray) do
+		if v[1] ~= -1 and v[2] ~= -1 then
+			tmp = math.abs(mobPosX - v[1]) + math.abs(mobPosY - v[2])
+			if tmp < nearestTarget then
+				nearestTarget = tmp
+				indexToRet = i
+			end
+		end
+	end
+	return (indexToRet - 1);
+end	
